3e46296e0447a7debd8a80bb25d7f5713b601bf7
boat718 <boat_loading@hotmail.com>
2024-06-12 17:59:04 +0700

    Done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { Product, Products } from 'src/types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
-import { PaginatorModule } from 'primeng/paginator';
+import { Paginator, PaginatorModule } from 'primeng/paginator';
+import { EditPopupComponent } from '../components/edit-popup/edit-popup.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule, PaginatorModule],
+  imports: [ProductComponent, CommonModule, 
+    PaginatorModule, EditPopupComponent, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -18,10 +21,54 @@ export class HomeComponent {
     private productService: ProductsService
   ) {}
 
+  @ViewChild('paginator') paginator: Paginator | undefined;
+
   products:Product[] = [];
 
   totalRecords:number = 0;
   rows:number = 5;
+
+  displayEditPopup:boolean = false;
+  displayAddPopup:boolean = false;
+
+  toggleEditPopup(product: Product) {
+    this.selectedProduct = product;
+    this.displayEditPopup = true;
+  }
+
+  toggleDeletePopup(product: Product) {
+    if (!product.id) {
+      return;
+    }
+
+    this.deleteProduct(product.id);
+  }
+
+  toggleAddPopup() {
+    this.displayAddPopup = true;
+  }
+
+  selectedProduct: Product = {
+    id: 0,
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+  };
+
+  onConfirmEdit(product: Product) {
+    if (!this.selectedProduct.id) {
+      return;
+    }
+
+    this.editProduct(product, this.selectedProduct.id);
+    this.displayEditPopup = false;
+  }
+
+  onConfirmAdd(product: Product) {
+    this.addProduct(product);
+    this.displayAddPopup = false;
+  }
 
   onProductOutput(product: Product) {
     console.log(product, 'Output');
@@ -29,6 +76,10 @@ export class HomeComponent {
 
   onPageChange(event: any) {
     this.fetchProuducts(event.page, event.rows);
+  }
+
+  resetPaginator() {
+    this.paginator?.changePage(0);
   }
 
   fetchProuducts(page: number, perPage:number) {
@@ -45,6 +96,7 @@ export class HomeComponent {
       next:(data) => {
         console.log(data);
         this.fetchProuducts(0, this.rows);
+        this.resetPaginator();
       },
       error:(err) => {
         console.log(err)
@@ -58,6 +110,7 @@ export class HomeComponent {
       next:(data) => {
         console.log(data);
         this.fetchProuducts(0, this.rows);
+        this.resetPaginator();
       },
       error:(err) => {
         console.log(err)
@@ -65,12 +118,13 @@ export class HomeComponent {
     })
   }
 
-  addProduct(product:Product, id:number) {
+  addProduct(product:Product) {
     this.productService.addProduct(`http://localhost:3000/clothes`, product)
     .subscribe({
       next:(data) => {
         console.log(data);
         this.fetchProuducts(0, this.rows);
+        this.resetPaginator();
       },
       error:(err) => {
         console.log(err)
